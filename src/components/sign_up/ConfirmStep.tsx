ac37dc4183da0f0350a267dca6527f8d61b64f24
Cllaude99 <xodbsrla1025@naver.com>
2025-04-08 18:41:03 +0900

    feat: 가입확인 스텝 구현

---
+++ src/components/sign_up/ConfirmStep.tsx
@@ -0,0 +1,54 @@
+import { SignUpStepData } from '@/pages/SignUp';
+import Button from '../common/button/Button';
+import StepMessage from './StepMessage';
+import { useNavigate } from 'react-router-dom';
+import { Input } from '../common/input/Input';
+import { getAgeFromBirthdate } from '@/utils/sign_up/getAgeFromBirthdate';
+import { isUnder14 } from '@/utils/sign_up/isUnder14';
+
+interface ConfirmStepProps {
+  signUpData: SignUpStepData;
+}
+
+export default function ConfirmStep({ signUpData }: ConfirmStepProps) {
+  const getGender = (gender: string) => {
+    if (gender === 'M') return '남성';
+    if (gender === 'F') return '여성';
+    return '기타';
+  };
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col h-full gap-3">
+      <StepMessage message={`입력한 내용을 확인해주세요`} />
+      <Input inputType="readOnly" label="이름" value={signUpData.name} />
+      <Input inputType="readOnly" label="지역" value={signUpData.location} />
+      <Input
+        inputType="readOnly"
+        label="생년월일"
+        value={signUpData.birthdate}
+      />
+      <Input
+        label="만나이"
+        value={`${getAgeFromBirthdate(signUpData.birthdate)}세`}
+        inputType="readOnly"
+      />
+      {isUnder14(signUpData.birthdate) && (
+        <Input
+          label="법정대리인 동의 여부"
+          value={signUpData.agreement ? '동의' : '미동의'}
+          inputType="readOnly"
+        />
+      )}
+      <Input
+        inputType="readOnly"
+        label="성별"
+        value={getGender(signUpData.gender)}
+      />
+      <Button onClick={() => navigate('/sign_up/complete')} className="mt-auto">
+        가입하기
+      </Button>
+    </div>
+  );
+}
