b8beeb7a0cbe312e89d20a6338b087dda28b10e2
Cllaude99 <xodbsrla1025@naver.com>
2025-04-08 18:40:09 +0900

    feat: 동의화면 구현

---
+++ src/components/sign_up/AgreementStep.tsx
@@ -0,0 +1,31 @@
+import Button from '../common/button/Button';
+import StepMessage from './StepMessage';
+
+interface AgreementStepProps {
+  onNext: () => void;
+  updateSignUpData: (agreement: boolean) => void;
+}
+
+export default function AgreementStep({
+  onNext,
+  updateSignUpData,
+}: AgreementStepProps) {
+  const agreement_description = `만 14세 미만의 사용자는 법정대리인(보호자)의 동의하에 당근을 이용할 수 있어요. (정보통신망법 제 31조 의거)`;
+
+  const handleAgreement = () => {
+    updateSignUpData(true);
+    onNext();
+  };
+
+  return (
+    <div className="flex flex-col h-full gap-3">
+      <StepMessage
+        message={`만 14세 미만의 사용자는\n법정대리인 동의가 필요해요`}
+      />
+      <h3>{agreement_description}</h3>
+      <Button onClick={handleAgreement} className="mt-auto">
+        법정대리인 본인인증
+      </Button>
+    </div>
+  );
+}
