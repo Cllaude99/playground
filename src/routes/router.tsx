5c26ec21d3edec4e8d886f7799ce84eeb0394bd8
Cllaude99 <xodbsrla1025@naver.com>
2025-04-08 18:37:12 +0900

    feat: route setting

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,9 +1,26 @@
+import Layout from '@/components/layout/Layout';
 import Home from '@/pages/Home';
+import SignUp from '@/pages/SignUp';
+import SignUpComplete from '@/pages/SignUpComplete';
 import { createBrowserRouter } from 'react-router-dom';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'sign_up',
+        element: <SignUp />,
+      },
+      {
+        path: 'sign_up/complete',
+        element: <SignUpComplete />,
+      },
+    ],
   },
 ]);
