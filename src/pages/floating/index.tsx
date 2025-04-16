e42ea322544f28df0cb9f51750a9283fb0cb7feb
Cllaude99 <xodbsrla1025@naver.com>
2025-04-16 18:38:06 +0900

    feat: FloatingWidget 컴포넌트에 useClickOutside 훅 적용 및 ref 추가

--- src/pages/floating/index.tsx
+++ src/pages/floating/index.tsx
@@ -1,13 +1,26 @@
+import { useRef } from 'react';
 import FloatingButton from '@/components/domain/floating/FloatingButton';
 import FloatingPanel from '@/components/domain/floating/FloatingPanel';
 import FloatingWidgetLayout from '@/components/domain/floating/FloatingWidgetLayout';
 import useToggle from '@/hooks/useToggle';
+import useClickOutside from '@/hooks/useClickOutside';
 
 export default function FloatingWidget() {
-  const { value: isOpen, toggleValue: toggleOpen } = useToggle(false);
+  const floatingRef = useRef<HTMLDivElement>(null);
+  const {
+    value: isOpen,
+    toggleValue: toggleOpen,
+    setValue: setIsOpen,
+  } = useToggle(false);
+
+  useClickOutside(floatingRef, () => {
+    if (isOpen) {
+      setIsOpen(false);
+    }
+  });
 
   return (
-    <FloatingWidgetLayout>
+    <FloatingWidgetLayout containerRef={floatingRef}>
       {isOpen && <FloatingPanel />}
       <FloatingButton isOpen={isOpen} onClick={toggleOpen} />
     </FloatingWidgetLayout>
