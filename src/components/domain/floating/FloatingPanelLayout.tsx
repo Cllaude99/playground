acab8844cb3eb2d3a3e58e692847e291e8332506
Cllaude99 <xodbsrla1025@naver.com>
2025-04-16 19:53:30 +0900

    style: FloatingPanelLayout 컴포넌트의 높이 및 클래스 이름 수정

--- src/components/domain/floating/FloatingPanelLayout.tsx
+++ src/components/domain/floating/FloatingPanelLayout.tsx
@@ -3,10 +3,10 @@ import { PropsWithChildren } from 'react';
 export default function FloatingPanelLayout({ children }: PropsWithChildren) {
   return (
     <div className="absolute bottom-24 right-0">
-      <div className="w-[390px] bg-white p-4 rounded-xl shadow-lg border-1 border-gray-200">
+      <div className="w-[390px] h-[calc(100dvh-450px)] bg-white p-4 rounded-xl shadow-lg border-1 border-gray-200">
         {children}
         <div
-          className="absolute w-4 h-4 bg-white right-8 -bottom-2 rotate-45 shadow-b-lg"
+          className="absolute size-4 bg-white right-8 -bottom-2 rotate-45 shadow-b-lg"
           style={{
             borderBottomRightRadius: '4px',
           }}
